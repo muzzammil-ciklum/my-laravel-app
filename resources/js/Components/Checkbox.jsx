7b18192de371414fcf1be95e6ac08bdb3ee6e9fc
muzzammil-ciklum <76935647+muzzammil-ciklum@users.noreply.github.com>
2024-10-10 10:49:59 +0500

    formating done

--- resources/js/Components/Checkbox.jsx
+++ resources/js/Components/Checkbox.jsx
@@ -2,11 +2,11 @@ export default function Checkbox({ className = '', ...props }) {
     return (
         <input
             {...props}
-            type="checkbox"
+            type='checkbox'
             className={
                 'rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 ' +
                 className
             }
         />
-    );
+    )
 }
--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -3,7 +3,7 @@ import {
     DialogPanel,
     Transition,
     TransitionChild,
-} from '@headlessui/react';
+} from '@headlessui/react'
 
 export default function Modal({
     children,
@@ -14,9 +14,9 @@ export default function Modal({
 }) {
     const close = () => {
         if (closeable) {
-            onClose();
+            onClose()
         }
-    };
+    }
 
     const maxWidthClass = {
         sm: 'sm:max-w-sm',
@@ -24,34 +24,34 @@ export default function Modal({
         lg: 'sm:max-w-lg',
         xl: 'sm:max-w-xl',
         '2xl': 'sm:max-w-2xl',
-    }[maxWidth];
+    }[maxWidth]
 
     return (
-        <Transition show={show} leave="duration-200">
+        <Transition show={show} leave='duration-200'>
             <Dialog
-                as="div"
-                id="modal"
-                className="fixed inset-0 z-50 flex transform items-center overflow-y-auto px-4 py-6 transition-all sm:px-0"
+                as='div'
+                id='modal'
+                className='fixed inset-0 z-50 flex transform items-center overflow-y-auto px-4 py-6 transition-all sm:px-0'
                 onClose={close}
             >
                 <TransitionChild
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
+                    enter='ease-out duration-300'
+                    enterFrom='opacity-0'
+                    enterTo='opacity-100'
+                    leave='ease-in duration-200'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
                 >
-                    <div className="absolute inset-0 bg-gray-500/75" />
+                    <div className='absolute inset-0 bg-gray-500/75' />
                 </TransitionChild>
 
                 <TransitionChild
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                    enterTo="opacity-100 translate-y-0 sm:scale-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enter='ease-out duration-300'
+                    enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+                    enterTo='opacity-100 translate-y-0 sm:scale-100'
+                    leave='ease-in duration-200'
+                    leaveFrom='opacity-100 translate-y-0 sm:scale-100'
+                    leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
                 >
                     <DialogPanel
                         className={`mb-6 transform overflow-hidden rounded-lg bg-white shadow-xl transition-all sm:mx-auto sm:w-full ${maxWidthClass}`}
@@ -61,5 +61,5 @@ export default function Modal({
                 </TransitionChild>
             </Dialog>
         </Transition>
-    );
+    )
 }
